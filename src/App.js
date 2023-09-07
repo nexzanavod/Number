0f2a5b0eae68617d9981e22f15b1ec789073bbcc
navod <navod@digitalxlabs.com>
2023-09-07 14:13:46 +0530

    New changes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  const phoneNumber = '0769099310'; // Replace with the desired phone number
+  const phoneNumber = '123-456-7890'; // Replace with the desired phone number
+
+  // Function to initiate a phone call
+  const callNumber = () => {
+    window.location.href = `tel:${phoneNumber}`;
+  };
 
   return (
     <div className="app-container">
       <h1>Your React App</h1>
       {/* Button to call a number */}
-      <a href={`tel:${phoneNumber}`}>
-        <button>Call Number</button>
-      </a>
+      <button className="button" onClick={callNumber}>
+        Call Number
+      </button>
     </div>
   );
 }
